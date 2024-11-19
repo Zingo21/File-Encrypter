24dc9a09100f35e3043e24e385a7d219650cd853
Albin Sandefalk <albinsand@hotmail.se>
2024-11-19 14:27:06 +0100

    Added button to manualy select file

--- Encrypter.py
+++ Encrypter.py
@@ -1,12 +1,12 @@
 from cryptography.fernet import Fernet
 import tkinter as tk
+from tkinter import filedialog
 import os
 import imghdr
 
 # TODO:
 # - ADD a way to encrypt/decrypt entire directories.
 # - ADD a way to manually select key file/path to create key file in.
-# - ADD a way to manually select file to encrypt/decrypt.
 
 if os.path.exists('file-encrypter.key') == False: # If file-encrypter.key file does not exist, create it.
     open('file-encrypter.key', 'x')
@@ -55,24 +55,40 @@ def decrypt_file(filename):
     with open(filename, 'wb') as file: # Write decrypted data to file.
         file.write(decrypted_data)
 
+def select_file():
+    if filepath.get() == "":
+        filepath_select = tk.filedialog.askopenfilename(initialdir=".", title="Select file", filetypes=(("all files", "*.*"), ("image files", "*.png;*.jpg;*.jpeg;*.gif;*.bmp;*.tiff;*.webp"), ("text files", "*.txt;*.docx;*.doc;*.pdf;*.rtf;*.odt;*.html;*.xml;*.csv;*.json;*.log;*.md;*.tex;*.yaml;*.yml;*.ini;*.cfg;*.conf;*.properties;*.env;*.sh;*.bat;*.cmd;*.ps1;*.psm1;*.psd1;*.ps1xml;*.pssc;*.psc1")))
+    else:
+        filepath_select = filepath.get()
+        
+    return filepath_select
+
 # GUI
 app = tk.Tk()
 app.title("File Encrypter")
-app.geometry("300x80")
+app.geometry("400x100")
 app.resizable(False, False)
 
-frame = tk.Frame(app)
-frame.pack()
-
-filepath_label = tk.Label(frame, text="Enter filepath:")
+label_frame = tk.Frame(app)
+label_frame.pack()
+filepath_label = tk.Label(label_frame, text="Enter filepath:")
 filepath_label.grid(row=0, column=0, pady=10)
-filepath = tk.Entry(frame)
+
+filepath = tk.Entry(label_frame)
 filepath.grid(row=0, column=1)
 
-encrypt_button = tk.Button(frame, text="Encrypt", command=lambda: encrypt_file(filepath.get()))
+select_button = tk.Button(label_frame, text="Select file", command=lambda: filepath.insert(0, select_file()))
+select_button.grid(row=0, column=2)
+
+#key_select = tk.filedialog.askopenfilename(initialdir=".", title="Select key file", filetypes=(("key files", "*.key"), ("all files", "*.*")))
+
+encrypt_decrypt_frame = tk.Frame(label_frame)
+encrypt_decrypt_frame.grid(row=1, column=1, pady=10)
+
+encrypt_button = tk.Button(encrypt_decrypt_frame, text="Encrypt", command=lambda: encrypt_file(filepath.get()))
 encrypt_button.grid(row=1, column=0, pady=0)
 
-decrypt_button = tk.Button(frame, text="Decrypt", command=lambda: decrypt_file(filepath.get()))
+decrypt_button = tk.Button(encrypt_decrypt_frame, text="Decrypt", command=lambda: decrypt_file(filepath.get()))
 decrypt_button.grid(row=1, column=1)
 
 app.mainloop()
